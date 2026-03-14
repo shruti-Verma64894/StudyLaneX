f59ac0d4d9945de9cc96ab2366a27353b4325791
Shruti <vshruti811@gmail.com>
2026-03-14 14:36:12 +0530

    fix cors for vercel

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,14 @@ const app = express();
 const contactRoutes = require("./routes/contactRoutes");
 
 // ===== Middlewares =====
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://study-lane-x.vercel.app"
+  ],
+  methods: ["GET", "POST"],
+  credentials: true
+}));
 app.use(express.json()); // convert request body into json
 // ===== Routes =====
 app.use("/api/feedback", feedbackRoutes);
