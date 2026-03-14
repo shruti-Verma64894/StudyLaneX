076f9338dfa0f71c12235adbf36822652fceef79
Shruti <vshruti811@gmail.com>
2026-03-14 15:43:55 +0530

    smtp fix

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -8,13 +8,15 @@ router.post("/send", async (req, res) => {
 
   try {
 
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-      }
-    });
+  const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
 
     const mailOptions = {
       from: email,
